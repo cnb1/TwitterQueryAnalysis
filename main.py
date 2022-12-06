3340d7ffb881f707731ef87bcc93ad53f0398a75
cnb1 <hackerrank134@gmail.com>
2022-12-06 10:46:19 -0500

    twitter api v2 start

--- main.py
+++ main.py
@@ -17,40 +17,19 @@ access_key = constants.ACCESS_TOKEN
 access_secret = constants.ACCESS_TOKEN_SECRET
 bearer_token = constants.BEARER_TOKEN
 
+def get_client():
+    return tweepy.Client(bearer_token=bearer_token, consumer_key=consumer_key, consumer_secret=consumer_secret,
+                            access_token=access_key, access_token_secret=access_secret,
+                            return_type=tweepy.Response, wait_on_rate_limit=False)
 
-def get_tweets(username):
-          
-        # Authorization to consumer key and consumer secret
-        auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-  
-        # Access to user's access key and access secret
-        auth.set_access_token(access_key, access_secret)
-  
-        # Calling api
-        api = tweepy.API(auth)
-  
-        # 200 tweets to be extracted
-        number_of_tweets=200
-        tweets = api.user_timeline(screen_name=username)
-  
-        # Empty Array
-        tmp=[] 
-  
-        # create array of tweet information: username, 
-        # tweet id, date/time, text
-        tweets_for_csv = [tweet.text for tweet in tweets] # CSV file created 
-        for j in tweets_for_csv:
-  
-            # Appending tweets to the empty array tmp
-            tmp.append(j) 
-  
-        # Printing the tweets
-        print(tmp)
-  
-  
+def getUser(username):
+    client = get_client()
+
+    user = client.get_user(username=username)
+
+    print(user)
+
 # Driver code
 if __name__ == '__main__':
-  
-    # Here goes the twitter handle for the user
-    # whose tweets are to be extracted.
-    get_tweets("devdevdev1001") 
+
+    getUser("devdevdev1001")
