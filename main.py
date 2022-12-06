04ead6b1a07250b24151e53a5df1c27e419437fd
cnb1 <hackerrank134@gmail.com>
2022-12-06 11:31:41 -0500

    tweet count

--- main.py
+++ main.py
@@ -11,16 +11,19 @@ import tweepy
 
 import constants
 
+#Store you corrent tokens and keys in a constants.py folder in the same directory
 consumer_key = constants.CONSUMER_KEY
 consumer_secret = constants.CONSUMER_KEY_SECRET
 access_key = constants.ACCESS_TOKEN
 access_secret = constants.ACCESS_TOKEN_SECRET
 bearer_token = constants.BEARER_TOKEN
 
-def get_client():
-    return tweepy.Client(bearer_token=bearer_token, consumer_key=consumer_key, consumer_secret=consumer_secret,
+client = tweepy.Client(bearer_token=bearer_token, consumer_key=consumer_key, consumer_secret=consumer_secret,
                             access_token=access_key, access_token_secret=access_secret,
                             return_type=tweepy.Response, wait_on_rate_limit=False)
+
+def get_client():
+    return client
 
 def getUser(username):
     client = get_client()
@@ -29,7 +32,16 @@ def getUser(username):
 
     print(user)
 
+def count_tweets(query):
+    client = get_client()
+
+    count = client.get_recent_tweets_count(query=query)
+
+    print(count.meta)
+
 # Driver code
 if __name__ == '__main__':
 
     getUser("devdevdev1001")
+
+    count_tweets("bitcoin")
